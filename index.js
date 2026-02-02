e8c849819eaf2618a1c170ff4faf3e954667a5e0
DNishadini <dinushikaherath150@gmail.com>
2026-02-02 23:17:11 +0530

    how to use request and response parameters

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", () => {
+app.get("/", (req, res) => {
+  console.log(req.body);
   console.log("Get request received");
+  res.json({
+    message: "Hello world",
+  });
 });
 
-app.post("/", () => {
+app.post("/", (req, res) => {
   console.log("Post request received");
+});
+
+app.delete("/", (req, res) => {
+  console.log("Delete request received");
 });
 
 app.listen(5000, () => {
