4621fd6579b38415ee6a9c797cee341ff483ef38
DNishadini <dinushikaherath150@gmail.com>
2026-03-15 00:47:04 +0530

    Authentication

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 import studentRouter from "./routes/studentsRouter.js";
 import userRouter from "./routes/userRouter.js";
@@ -7,6 +8,24 @@ import userRouter from "./routes/userRouter.js";
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  let token = req.header("Authorization");
+  if (token != null) {
+    token = token.replace("Bearer ", "");
+    jwt.verify(token, "jwt-secret", (err, decoded) => {
+      if (decoded == null) {
+        res.json({
+          message: "Invalid token please login again  ",
+        });
+        return;
+      } else {
+        req.user = decoded;
+      }
+    });
+    next();
+  }
+});
 
 const connectionString =
   "mongodb+srv://admin:123@cluster0.ocdf7bj.mongodb.net/?appName=Cluster0";
