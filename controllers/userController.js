31d74e55b6f9a75d7d69ef9f77919c9cd08a841d
DNishadini <dinushikaherath150@gmail.com>
2026-03-14 23:43:27 +0530

    create  jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
+import e from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export function createUser(req, res) {
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
@@ -21,4 +23,41 @@ export function createUser(req, res) {
         message: "Error creating user",
       });
     });
+}
+
+export function loginUser(req, res) {
+  User.findOne({
+    email: req.body.email,
+  }).then((user) => {
+    if (user == null) {
+      res.json({
+        message: "User not found",
+      });
+    } else {
+      const isPasswordMatching = bcrypt.compareSync(
+        req.body.password,
+        user.password,
+      );
+      if (isPasswordMatching) {
+        const token = jwt.sign(
+          {
+            email: user.email,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            role: user.role,
+            isEmailVerified: user.isEmailVerified,
+          },
+          "jwt-secret",
+        );
+        res.json({
+          message: "Login successful",
+          token: token,
+        });
+      } else {
+        res.json({
+          message: "Incorrect password",
+        });
+      }
+    }
+  });
 }
