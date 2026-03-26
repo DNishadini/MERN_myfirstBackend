91cd5f00dd08df7fd53aa17823647eb9abbdca1b
DNishadini <dinushikaherath150@gmail.com>
2026-03-26 08:25:20 +0530

    Create product models and controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,3 +36,76 @@ export async function getProducts(req, res) {
     });
   }
 }
+
+export async function deleteProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "Only admin can delete product",
+    });
+    return;
+  }
+  try {
+    const productID = req.params.productId;
+    await Product.deleteOne({
+      productID: productID,
+    });
+    res.json({
+      message: "Product deleted successfully",
+    });
+  } catch (err) {
+    console.error("Error deleting product:", err);
+    res.status(500).json({
+      message: "Error deleting product",
+    });
+  }
+}
+
+export async function updateProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "Only admin can update product",
+    });
+    return;
+  }
+  try {
+    const productID = req.params.productId;
+    const updateData = req.body;
+
+    await Product.updateOne(
+      {
+        productID: productID,
+      },
+      updateData,
+    );
+    res.json({
+      message: "Product updated successfully",
+    });
+  } catch (err) {
+    console.error("Error updating product:", err);
+    res.status(500).json({
+      message: "Error updating product",
+    });
+  }
+}
+
+export async function getProductID(req, res) {
+  try {
+    const productID = req.params.productId;
+    const product = await Product.findOne({
+      productID: productID,
+    });
+
+    if (product == null) {
+      res.status(404).json({
+        message: "Product not found",
+      });
+    } else {
+      res.json(product);
+    }
+  } catch (err) {
+    console.error("Error fetching product:", err);
+    res.status(500).json({
+      message: "Error fetching product",
+    });
+  }
+}
